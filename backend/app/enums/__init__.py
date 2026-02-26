aa921c85c84d0ebbf0dc12fffad98868ffcd7def
jf <jfgm299@gmail.com>
2026-02-26 12:04:41 +0100

    refactor(expenses_tracker): Enums moved into module folder

--- backend/app/enums/__init__.py
+++ backend/app/enums/__init__.py
@@ -1,5 +1,4 @@
-from .expense_category import ExpenseCategory
 from .muscle_group_category import MuscleGroupCategory
 from .gym_set_type import GymSetType
 
-__all__ = [ExpenseCategory, MuscleGroupCategory, GymSetType]
+__all__ = [MuscleGroupCategory, GymSetType]
--- backend/app/enums/expense_category.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum as PyEnum
-
-class ExpenseCategory(str, PyEnum):
-    REVOLUT = 'Revolut',
-    IMAGIN = 'Imagin'
