c67ad1c32a480d67271f92f0ee9140732adee89d
jf <jfgm299@gmail.com>
2026-03-06 20:53:10 +0100

    fix(alembic)

--- backend/alembic/versions/c92cac1ce388_add_calendar_tracker.py
+++ backend/alembic/versions/c92cac1ce388_add_calendar_tracker.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'c92cac1ce388'
-down_revision: Union[str, Sequence[str], None] = 'fe20405602f3'
+down_revision: Union[str, Sequence[str], None] = 'normalize_gymsettype'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
